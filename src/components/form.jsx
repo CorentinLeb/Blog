a550733073761a985202e7ff13379ab7128be4bf
Sébastien Avenet <sebastien.avenet1@gmail.com>
2021-02-24 08:43:41 +0100

    modif form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,18 +2,28 @@ import '../css/form.css'
 import React, { useState } from 'react'
 
 
+
 const Form = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmit(true);
+        setPost(title,content,tags);
+        console.log(post)
+
+    }
 
     const [title,setTitle]= useState('');
     const [content,setContent]= useState('');
     const [tags,setTags]= useState([]);
-    const [submit,setSubmit]= useState(false)
+    const [submit,setSubmit]= useState(false);
+    const [post,setPost]= useState({})
     
     
     return (
         <div>
-        <form>
-                <div className='post-form'>
+        <form onSubmit={handleSubmit}>
+                <div className='post-form' >
                     <label htmlFor="post-title">Titre du post</label>
                     <input type="text" name="post-title" id="post-title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
                     <label htmlFor="post-content">Contenu : </label>
@@ -22,7 +32,7 @@ const Form = () => {
                     <input type="text" name="post-tags" id="post-tags" value={tags} onChange={(e)=>
                         {let newTags = e.target.value.split(',');
                             setTags(newTags)}}/>
-                    <input type='sumit' value={submit} />
+                    <input type='submit' value='Go' />
 
                 </div>
 
